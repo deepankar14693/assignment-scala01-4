e76b5b62777d9d5f95e80788f51b1eb71f8232eb
deepankar <deepankar.ranswal@knoldus.in>
2018-01-11 18:02:38 +0530

    assignment submitted

--- src/main/scala/com/knoldus/HighOrderFunctionsImplementation.scala
+++ src/main/scala/com/knoldus/HighOrderFunctionsImplementation.scala
@@ -7,4 +7,5 @@ class HighOrderFunctionsImplementation {
  def cubeCalculation(firstval: Int,  f: (Int) => Int): Int = f(firstval)
 
  def sumCalculation(firstval: Int,secondval: Int,f: (Int,Int) => Int): Int = f(firstval,secondval)
+
 }
